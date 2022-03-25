6bafb5fefc3560196c63e07dfb025831417c3416
Denis <kanukov.denis@gmail.com>
2022-03-25 19:22:22 +0300

    add logic

--- js/index.js
+++ js/index.js
@@ -1,11 +1,37 @@
 const cards = document.querySelectorAll('.card');
 let firstFlippedCard;
-let secondFlippedCard
+let secondFlippedCard;
+let haveFlippedCard = false;
 
 cards.forEach(element => {
     element.addEventListener('click', function name() {
-        if (!firstFlippedCard) {
-            element.classList.add('flipped');
+        element.classList.add('flipped');
+
+        if (!haveFlippedCard) {
+            haveFlippedCard = true;
+            firstFlippedCard = element;
+            return;
+        } else {
+            secondFlippedCard = element;
+        }
+
+        haveFlippedCard = false;
+        if (!checkPair(firstFlippedCard, secondFlippedCard)) {
+            setTimeout(removeFlip, 1000, firstFlippedCard, secondFlippedCard)
         }
     })
 });
+
+function checkPair(firstCard, secondCard) {
+    if (firstCard.dataset.num === secondCard.dataset.num) {
+        lockPair(firstCard, secondCard);
+        return true;
+    }
+    return false;
+}
+
+function removeFlip(firstCard, secondCard) {
+    firstCard.classList.remove('flipped');
+    secondCard.classList.remove('flipped');
+    console.log(firstCard.classList, secondCard.classList)
+}
